528b5f4abf72e0ac1fac58fed764b99ac81a1350
Ciurchil-Giurgiu Celia <celia.ciurchil@student.unitbv.ro>
2025-02-27 13:00:30 +0200

    Added project

---
+++ Tema3CNN/Tema3CNN/Models/CNNModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+namespace Tema3CNN.Models
+{
+    public class CNNModel
+    {
+        private InferenceSession _session;
+
+        public CNNModel(string modelPath)
+        {
+            try
+            {
+                _session = new InferenceSession(modelPath);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"Eroare la încărcarea modelului ONNX: {ex.Message}");
+                throw;
+            }
+        }
+
+        public int Predict(float[] input)
+        {
+            try
+            {
+                var inputTensor = new DenseTensor<float>(input, new[] { 1, 3, 32, 32 });
+                var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("input", inputTensor) };
+                var results = _session.Run(inputs);
+                var output = results.First().AsEnumerable<float>().ToArray();
+                return Array.IndexOf(output, output.Max()); 
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"Eroare la inferență: {ex.Message}");
+                throw;
+            }
+        }
+
+
+        public static float[] ProcessImage(string imagePath)
+        {
+            try
+            {
+                using var bmp = new Bitmap(imagePath);
+                var resized = new Bitmap(bmp, new Size(32, 32));
+
+                float[] normalized = new float[3 * 32 * 32];
+
+                for (int c = 0; c < 3; c++) 
+                {
+                    for (int y = 0; y < 32; y++)
+                    {
+                        for (int x = 0; x < 32; x++)
+                        {
+                            Color pixel = resized.GetPixel(x, y);
+                            float value = c switch
+                            {
+                                0 => pixel.R,   
+                                1 => pixel.G,   
+                                2 => pixel.B,   
+                                _ => 0
+                            };
+
+                            normalized[c * 32 * 32 + y * 32 + x] = (value / 255.0f - 0.5f) / 0.5f;
+                        }
+                    }
+                }
+
+                return normalized;
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"Eroare la procesarea imaginii: {ex.Message}");
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Tema3CNN/Tema3CNN/ViewModels/MainViewModel.cs
@@ -0,0 +1,130 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Tema3CNN.Models;
+
+namespace Tema3CNN.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private CNNModel _model;
+        private string _prediction;
+        private BitmapImage _loadedImage;
+
+        public string Prediction
+        {
+            get => _prediction;
+            set
+            {
+                _prediction = value;
+                OnPropertyChanged(nameof(Prediction));
+            }
+        }
+
+        public BitmapImage LoadedImage
+        {
+            get => _loadedImage;
+            set
+            {
+                _loadedImage = value;
+                OnPropertyChanged(nameof(LoadedImage));
+            }
+        }
+
+        public ICommand LoadImageCommand { get; }
+        public ICommand PredictCommand { get; }
+
+        private string _imagePath;
+
+        public MainViewModel()
+        {
+            _model = new CNNModel("cifar10_mobileNet.onnx");
+            LoadImageCommand = new RelayCommand(LoadImage);
+            PredictCommand = new RelayCommand(Predict);
+        }
+
+        private void LoadImage()
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Image Files (*.png;*.jpg)|*.png;*.jpg"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _imagePath = openFileDialog.FileName;
+
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(_imagePath);
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+
+                LoadedImage = bitmap;
+            }
+        }
+
+        private void Predict()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(_imagePath))
+                {
+                    System.Windows.MessageBox.Show("Vă rugăm să încărcați o imagine înainte de a prezice.");
+                    return;
+                }
+
+                var input = CNNModel.ProcessImage(_imagePath);
+                var result = _model.Predict(input);
+                string prediction="";
+                switch(result)
+                {
+                    case 0:
+                        prediction = "Avion";
+                        break;
+                    case 1:
+                        prediction = "Automobil";
+                        break;
+                    case 2:
+                        prediction = "Pasăre";
+                        break;
+                    case 3:
+                        prediction = "Pisică";
+                        break;
+                    case 4:
+                        prediction = "Căprioară";
+                        break;
+                    case 5:
+                        prediction = "Câine";
+                        break;
+                    case 6:
+                        prediction = "Broască";
+                        break;
+                    case 7:
+                        prediction = "Cal";
+                        break;
+                    case 8:
+                        prediction = "Navă";
+                        break;
+                    case 9:
+                        prediction = "Camion";
+                        break;
+                    default:
+                        break;
+                }
+
+                 Prediction = $"Predicție: {prediction}";
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"Eroare la predicție: {ex.Message}");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
